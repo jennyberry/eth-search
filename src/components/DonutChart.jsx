37392e1fc0e1c6fa42d8d4f1e7a80532713b95af
jennypham <64534681+jennyberry@users.noreply.github.com>
2023-02-21 20:21:37 +1100

    donut figure

--- src/components/DonutChart.jsx
+++ src/components/DonutChart.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+/* import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Doughnut } from 'react-chartjs-2'
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(ArcElement, Tooltip, Legend) */
+
+
+import Chart from 'react-apexcharts';
 
 const DonutChart = () => {
-    const data = {
+   /*  const data = {
         labels: ['Distrbuted', 'Remaining'],
         datasets: [{
             label: 'Funding',
@@ -14,16 +17,118 @@ const DonutChart = () => {
         }]
     }
     const options = {
+        plugins: {
+            legend: {
+                display: false
+            }
+       }
 
     }
+    const summary = {
+        id: 'summary',
+        beforeDatasetsDraw(chart, args, pluginOptions) {
+            const { ctx, data } = chart
+            ctx.save()
+            ctx.font = 'bold 12px sans-serif';
+            ctx.fillStyle = 'black'
+            ctx.textAlign = 'center'
+            ctx.textBaseline = 'middle'
+            ctx.fillText( `38,5M <br/> total fund requested`,chart.getDatasetMeta(0).data[0].x, chart.getDatasetMeta(0).data[0].y)
+
+            // ctx.fillText(`${data.datasets[0].data[0]} total fund requested`,chart.getDatasetMeta(0).data[0].x, chart.getDatasetMeta(0).data[0].y)
+        }
+    } */
   return (
       <div className='max-w-[400px]'>
-          <Doughnut
+         {/*  <Doughnut
           data = {data}
-          options = {options}
+              options={options}
+              plugins = {[summary]}
           >
 
-          </Doughnut>
+          </Doughnut> */}
+<React.Fragment>
+            <div className='container-fluid mt-3 mb-3'>
+                  <Chart
+                      type="donut"
+                      width={450}
+                      height={450}
+                      series={[25900000, 12400000]}
+
+                      options={{
+                          labels: ['Distributed', 'Remaining'],
+                          legend: {
+                              show: false,
+                              position: "bottom",
+                              labels: {
+                                colors: ['#5368F3', '#FFC403'],
+                                useSeriesColors: false,
+                            },
+                          },
+                          fill: {
+                              colors: ['#5368F3', '#FFC403']
+                          },
+                          tooltip: {
+                            enabled: true,
+                              fillSeriesColor: true,
+                              style: {
+                                fontSize: '16px',
+                                fontFamily: 'nunito'
+                              },
+                              followCursor: true,
+
+                          },
+                          colors: ['#5368F3', '#FFC403'],
+                          states: {
+                            hover: {
+                                filter: {
+                                    type: 'lighten',
+                                    value:'0.1'
+                                }
+                            },
+},
+            //  title:{
+            //     text:"Funding",
+            //    align:"center",
+            //  },
+
+             plotOptions:{
+             pie:{
+                     donut: {
+
+                     labels: {
+                             show: true,
+                             value: {
+                                show: true,
+                                fontSize: '16px',
+                                colors: '#f90000',
+                       },
+                        total:{
+                            show:true,
+                            // showAlways:true, //show number when hover
+                             //formatter: () => '343',
+                            fontSize: 30,
+                            fontFamily: 'nunito'
+                            // color: '#f90000',
+                             },
+
+                    }
+                }
+             }
+
+             },
+
+             dataLabels:{
+                enabled:false, //percentage on the chart
+             }
+
+
+            }}
+
+            />
+
+            </div>
+        </React.Fragment>
     </div>
   )
 }
